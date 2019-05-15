92510fbeb148f17a2264146582539a040454fd33
wangfeizhou <18819466397@163.com>
2019-05-15 11:09:51 +0800

    first commit

---
+++ src/util/api.js
@@ -0,0 +1,18 @@
+export default {
+  // host: 'http://127.0.0.1:4040',
+  // host: 'http://39.108.63.38:4040',
+  host: 'https://api.rgznyddx.com',
+
+
+  login: '/api/wx/check',
+  register: '/api/user/register',
+
+  getReport: '/api/user/get/report/',
+
+  uploadUserPhoto: '/api/cos/upload/userPhoto',
+
+  getNewsByType: '/api/news/get',
+  getNewsDetail: '/api/news/detail',
+
+  getVideosByType: '/api/videos/get'
+}
